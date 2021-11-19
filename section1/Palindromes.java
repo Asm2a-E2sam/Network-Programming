028ca6e55bb9d5fc5bf7a1cf7eaa110a46402384
Asmaa E Soliman <94235540+Asm2a-E2sam@users.noreply.github.com>
2021-11-19 23:37:10 +0200

    Update Palindromes.java

--- section1/Palindromes.java
+++ section1/Palindromes.java
@@ -5,22 +5,34 @@ import java.util.Scanner;
 public class Palindromes {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String str ;
-        System.out.print("Enter a String: ");
-        str = input.nextLine();
-        boolean flag =true;
-        int size = str.length();
+        String str = "";
 
-        for (int i = 0; i < size/2; i++) {
-            if (str.charAt(i) != str.charAt(size-(i+1))) {
-                flag = false;
+        while (true){
+            System.out.println("\n***************************************");
+            System.out.println("Enter \"CLOSE\" if you want to leave...");
+            System.out.println("***************************************");
+
+            System.out.println("Enter a String: ");
+            str = input.nextLine();
+            if (str.equals("CLOSE")) {
+                break;
             }
+            boolean flag =true;
+            int size = str.length();
+
+            for (int i = 0; i < size/2; i++) {
+                if (str.charAt(i) != str.charAt(size-(i+1))) {
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+                System.out.println(str + " is a Palindromes");
+            }else {
+                System.out.println(str + " is not a Palindromes");
+            }
+
         }
 
-        if (flag) {
-            System.out.println(str + " is a Palindromes");
-        }else {
-            System.out.println(str + " is not a Palindromes");
-        }
     }
 }
